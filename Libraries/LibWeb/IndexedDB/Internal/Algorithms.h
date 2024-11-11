72fe175d6887e536c57f5b1964fafa3bd45ae2e8
stelar7 <dudedbz@gmail.com>
2024-11-11 10:49:36 +0100

    LibWeb: Implement convert_a_value_to_a_key for IndexedDB

--- Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
+++ Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
@@ -8,11 +8,13 @@
 
 #include <LibJS/Runtime/Realm.h>
 #include <LibWeb/IndexedDB/IDBRequest.h>
+#include <LibWeb/IndexedDB/Internal/Key.h>
 #include <LibWeb/StorageAPI/StorageKey.h>
 
 namespace Web::IndexedDB {
 
 WebIDL::ExceptionOr<JS::NonnullGCPtr<IDBDatabase>> open_a_database_connection(JS::Realm&, StorageAPI::StorageKey, String, Optional<u64>, JS::NonnullGCPtr<IDBRequest>);
 bool fire_a_version_change_event(JS::Realm&, FlyString const&, JS::NonnullGCPtr<DOM::EventTarget>, u64, Optional<u64>);
+ErrorOr<Key> convert_a_value_to_a_key(JS::Realm&, JS::Value, Vector<JS::Value> = {});
 
 }
